1556fa08c089c8166f6b088f98755b2498e0b653
Ким Никита Вячеславович <kim-nikita202@mail.ru>
2023-03-26 15:11:56 +0300

    mini fix

--- src/components/ResortPage.vue
+++ src/components/ResortPage.vue
@@ -205,4 +205,15 @@ export default {
     font-size: 1.25rem;
   }
 }
+
+input[type="date"], input[type="number"], select {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
 </style>
