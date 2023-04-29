65a67e713d2ef39c41cd2be1c80733c0af62c042
Nana Pisanko <bonjozzy@gmail.com>
2023-04-30 00:32:57 +0300

    fix-merge: fix styles booking pages

--- src/components/BookingPage.vue
+++ src/components/BookingPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="isLoggedIn" class="my-booking">
-    <h3 class="my-booking-title">Мои бронирования:</h3>
+    <h3 class="my-booking__title">Мои бронирования:</h3>
     <booking-item v-for="item in booking"
                   :key="item.id"
                   :item="item"
@@ -62,7 +62,7 @@ export default {
       }
     } catch (err) {
       console.error(err);
-    }*/
+    }
 
     /* if(!this.isLoggedIn) {
        this.$router.push('/login');
@@ -73,31 +73,5 @@ export default {
 }
 </script>
 <style>
-.my-booking {
-  padding: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 0.5rem;
-}
 
-.my-booking-title {
-  margin-top: 0;
-  font-size: 1.5rem;
-  font-weight: bold;
-}
-
-.my-booking-item-body p {
-  margin: 0;
-  font-size: 1.1rem;
-}
-
-@media (max-width: 767px) {
-  .my-booking-title {
-    font-size: 1.25rem;
-  }
-
-
-  .my-booking-item-body p {
-    font-size: 0.9rem;
-  }
-}
 </style>
