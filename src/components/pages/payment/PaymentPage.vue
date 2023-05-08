5e28c0b2f199045acc0196b1a9117049be457f4d
Nana Pisanko <bonjozzy@gmail.com>
2023-05-08 19:26:20 +0300

    fix: PaymentPage.vue

--- src/components/pages/payment/PaymentPage.vue
+++ src/components/pages/payment/PaymentPage.vue
@@ -90,8 +90,8 @@ export default {
     }
   },
   created() {
-    if(localStorage.getItem('firstName').length === 0) {
-      console.log(localStorage.getItem('firstName').length);
+    if(!localStorage.getItem('firstName')){
+      console.log('Сначала нужно авторизоваться')
     }
     this.total = this.$route.query.total;
     /*
