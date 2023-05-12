f8d9dfc08585a4d93f29075410b0818cf88ec5eb
Ким Никита Вячеславович <kim-nikita202@mail.ru>
2023-05-12 23:58:29 +0300

    up

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8081',
+                target: 'http://158.160.36.229:8081',
                 changeOrigin: true
             }
         }
